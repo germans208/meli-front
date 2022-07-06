c47077acacafe9861646e8926528d4158efb78e4
germans208 <german.settino@gmail.com>
2022-07-06 17:46:05 -0300

    styles add logo meli

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./_navbar.scss";
 
+import ImageSearchMeli from "../../assets/search-meli.png";
+import Logo from "../../assets/logo.svg";
 const NavBar = () => {
   return (
     <div>
       <header className="nav-header" role="banner">
         <div className="nav-bounds">
-          <a className="nav-logo" href="/" />
+          <a className="nav-logo" href="/">
+            <img alt="logo" src={Logo} title="logo" />
+          </a>
 
           <form className="nav-search">
             <input
@@ -17,8 +21,8 @@ const NavBar = () => {
               type="text"
             />
 
-            <button className="nav-search-btn" type="submit">
-              Buscar
+            <button className="nav-search-btn" onClick={(e) => console.log(e)}>
+              <img alt="" src={ImageSearchMeli} />
             </button>
           </form>
         </div>
