623ee5408cc2ba53903d7469bf363153dad4bf4a
germans208 <german.settino@gmail.com>
2022-07-11 18:07:09 -0300

    feature add seo in home page

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import Helmet from "react-helmet";
 
+import logo from "../../assets/logo.svg";
 import NavBar from "../../components/NavBar/NavBar";
-
 const Home = () => {
   return (
     <main>
       <NavBar />
+
+      <Helmet>
+        <title>Home | Mercado libre</title>
+        <meta
+          content="La comunidad de compra y venta online más grande de América Latina."
+          name="description"
+        />
+        <meta content="Home | Mercado libre" property="og:title" />
+        <meta content={logo} property="og:image" />
+        <meta
+          content="La comunidad de compra y venta online más grande de América Latina."
+          property="og:description"
+        />
+      </Helmet>
     </main>
   );
 };
